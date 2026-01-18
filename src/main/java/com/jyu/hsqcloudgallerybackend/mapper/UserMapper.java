71cea9b5cd870be704fd879539ff7c9c8aa84597
minH_IT <1878427982@qq.com>
2026-01-18 12:01:31 +0800

    dev：用户模块开发

---
+++ src/main/java/com/jyu/hsqcloudgallerybackend/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.jyu.hsqcloudgallerybackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jyu.hsqcloudgallerybackend.model.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
